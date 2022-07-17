4e0e7f2068075614d3997a54d0be58555f414892
MaverickSamurai <mirsamirba@code.edu.az>
2022-07-17 16:22:41 +0400

    Login Exception

---
+++ LoginException/LoginException/Exception/LoginAuther.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace LoginException.Login
+{
+    public class Authentification : Exception
+    {
+        public static string Mail = "mirsamirba@code.edu.az";
+        public static string Password = "Yambaleyy7895";
+        public Authentification(string message) : base(message)
+        {
+
+        }
+
+    }
+}
---
+++ LoginException/LoginException/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using LoginException.Login;
+using System;
+
+namespace LoginException
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LoginAuther(Authentification.Mail, Authentification.Password) ;
+            
+        }
+
+
+        static void LoginAuther(string mail, string password)
+        {
+            try
+            {
+                if (mail != Authentification.Mail || password != Authentification.Password)
+                {
+                    throw new Authentification("Mail ve ya Password sehvdir");
+                }
+                Console.WriteLine("Giris olundu");
+            }
+            catch (Exception message)
+            {
+
+                Console.WriteLine(message.Message); ;
+            }
+        }
+
+    }
+}
